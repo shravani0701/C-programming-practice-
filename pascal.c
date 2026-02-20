22dba78f035651be895e866b1e6ada8de420420c
Sravani Maddikuntla <shravanimaddikuntla18@gmail.com>
2026-02-20 21:04:08 +0530

    Add files via upload

---
+++ pascal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        int num = 1;
+
+        for(int space = 0; space < n - i; space++)
+            printf(" ");
+
+        for(int j = 0; j <= i; j++) {
+            printf("%d ", num);
+            num = num * (i - j) / (j + 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
